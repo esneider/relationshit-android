87c9938aacab3ae6ca819cbbd1f1a824451833de
esneider <dariosn@gmail.com>
2014-12-31 11:56:45 +0200

    Add topLists interface

--- app/src/main/java/hack/relationshit/http/ServerDAO.java
+++ app/src/main/java/hack/relationshit/http/ServerDAO.java
@@ -3,15 +3,19 @@ package hack.relationshit.http;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.apache.http.Header;
 
 import hack.relationshit.R;
 import hack.relationshit.ShitApplication;
 import org.apache.http.entity.StringEntity;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 
@@ -19,7 +23,7 @@ import java.io.UnsupportedEncodingException;
  * Created by dario on 12/30/14.
  */
 public class ServerDAO {
-    public static void sendMessageList(String IMEI, Message[] messages, final Activity activity) {
+    public static void sendMessageList(final String IMEI, Message[] messages, final Activity activity) {
 
         Gson gson = new Gson();
 
@@ -38,12 +42,32 @@ public class ServerDAO {
         RestClient.post("messageList", entity, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Log.e("post", responseBody.toString());
+                getTopLists(IMEI, activity);
                 shit.messagesHaveLoaded(activity, true);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 shit.messagesHaveLoaded(activity, false);
+            }
+        });
+    }
+
+    public static void getTopLists(String IMEI, final Activity activity) {
+
+        final ShitApplication shit = (ShitApplication) activity.getApplicationContext();
+
+        RestClient.get("topLists", null, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray array) {
+                Log.e("get SUCC", array.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable error) {
+                Log.e("get FAIL", responseString);
             }
         });
     }
